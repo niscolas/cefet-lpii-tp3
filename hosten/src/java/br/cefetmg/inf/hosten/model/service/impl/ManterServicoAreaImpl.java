88a8d56991bc053b44d31bfbb89c1e49b03b51ca
Nícolas (aka. Gzus) <nic.cp@hotmail.com>
2018-08-14 20:04:46 -0300

    Corrige nomes de classes 'AreaServico' para 'ServicoArea'

--- hosten/src/java/br/cefetmg/inf/hosten/model/service/impl/ManterServicoAreaImpl.java
+++ hosten/src/java/br/cefetmg/inf/hosten/model/service/impl/ManterServicoAreaImpl.java
@@ -1,50 +1,53 @@
 package br.cefetmg.inf.hosten.model.service.impl;
 
-import br.cefetmg.inf.hosten.model.service.ManterAreaServico;
+import br.cefetmg.inf.hosten.model.dao.impl.ServicoAreaDAO;
+import br.cefetmg.inf.hosten.model.domain.ServicoArea;
 import br.cefetmg.inf.util.exception.NegocioException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import br.cefetmg.inf.hosten.model.service.ManterServicoArea;
 
-public class ManterAreaServicoImpl implements ManterAreaServico {
+public class ManterServicoAreaImpl implements ManterServicoArea {
 
     ServicoAreaDAO objetoDAO;
     
-    public ManterAreaServicoImpl() {
+    public ManterServicoAreaImpl() {
         objetoDAO = ServicoAreaDAO.getInstance();
     }
     
-
     @Override
-    public boolean inserir(ServicoArea servicoArea) {
-        //
-        //
-        //
+    public boolean inserir(ServicoArea servicoArea)
+            throws NegocioException, SQLException {
+        //TODO
+        return false;
     }
 
     @Override
-    public boolean alterar(String codRegistro, ServicoArea servicoArea) {
-        //
-        //
-        //
+    public boolean alterar(String codRegistro, ServicoArea servicoArea)
+            throws NegocioException, SQLException {
+        //TODO
+        return false;
     }
 
     @Override
-    public boolean excluir(String codRegistro) throws NegocioException {
-        //
-        //
-        //
+    public boolean excluir(String codRegistro)
+            throws NegocioException, SQLException {
+        //TODO
+        return false;
     }
 
     @Override
-    public Object buscar(Object dadoBusca, String coluna) {
-        //
-        //
-        //
+    public List<ServicoArea> listar(Object dadoBusca, String coluna)
+            throws NegocioException, SQLException {
+        //TODO
+        return new ArrayList<>();
     }
 
     @Override
-    public Object buscarTodos() {
-        //
-        //
-        //
+    public List<ServicoArea> listarTodos()
+            throws NegocioException, SQLException {
+        //TODO
+        return new ArrayList<>();
     }
-    
 }
