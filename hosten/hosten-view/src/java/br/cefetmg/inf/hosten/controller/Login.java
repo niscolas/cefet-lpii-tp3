75b0f59b133eb20269976e4f4671dbd358e8ff8a
Nícolas (aka. Gzus) <nic.cp@hotmail.com>
2018-08-20 10:55:16 -0300

    Atualiza estrutura de pastas para padrão a La Maffort
    Dentro da pasta 'hosten' existem agora as pastas 'hosten-commom', 'hosten-core' e 'hosten-view'

---
+++ hosten/hosten-view/src/java/br/cefetmg/inf/hosten/controller/Login.java
@@ -0,0 +1,43 @@
+package br.cefetmg.inf.hosten.controller;
+
+import br.cefetmg.inf.hosten.model.domain.Usuario;
+import br.cefetmg.inf.hosten.model.service.IManterUsuario;
+import br.cefetmg.inf.hosten.model.service.impl.ManterUsuario;
+import javax.servlet.http.HttpServletRequest;
+
+public class Login {
+
+    private static String email;
+    private static String senha;
+    
+    @SuppressWarnings("static-access")
+    public static String execute(HttpServletRequest request) {
+
+        String jsp = "";
+
+        try {
+            email = request.getParameter("email");
+            senha = request.getParameter("password");
+
+            IManterUsuario manterUsuario = new ManterUsuario();
+            Usuario usr = manterUsuario.usuarioLogin(email, senha);
+
+            if (usr == null) {
+                String erro = "Usuário nao encontrado!";
+                request.setAttribute("erro", erro);
+                jsp = "/erro.jsp";
+            } else {
+                request.getSession().setAttribute("desEmail", usr.getDesEmail());
+                request.getSession().setAttribute("codCargo", usr.getCodCargo());
+                request.getSession().setAttribute("codUsuario", usr.getCodUsuario());
+                request.getSession().setAttribute("nomUsuario", usr.getNomUsuario());
+                jsp = "/view/quartos-estados.jsp";
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+}
---
+++ hosten/hosten-view/src/java/br/cefetmg/inf/hosten/controller/inserir/InserirItemConforto.java
@@ -0,0 +1,34 @@
+package br.cefetmg.inf.hosten.controller.inserir;
+
+import br.cefetmg.inf.hosten.model.domain.ItemConforto;
+import br.cefetmg.inf.hosten.model.service.impl.ManterItemConforto;
+import javax.servlet.http.HttpServletRequest;
+import br.cefetmg.inf.hosten.model.service.IManterItemConforto;
+
+public class InserirItemConforto {
+
+    public static String execute(HttpServletRequest request) {
+        String jsp = "";
+        try {
+            IManterItemConforto manterItem = new ManterItemConforto();
+            
+            String codItem = request.getParameter("codItem");
+            String desItem = request.getParameter("desItem");
+            
+            ItemConforto itemConforto = new ItemConforto(codItem, desItem);
+            boolean testeRegistro = manterItem.inserir(itemConforto);
+            
+            if (testeRegistro)
+                request.setAttribute("mensagem", "Item inserido com sucesso");
+            else
+                request.setAttribute("mensagem", "Não foi possível inserir o item!");
+            
+            jsp = "/servletweb?acao=ListarItensConforto";
+        } catch (Exception e) {
+            e.printStackTrace();
+            jsp = "";
+        }
+        return jsp;
+    }
+    
+}
